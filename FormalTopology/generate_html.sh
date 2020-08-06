004648c99a3a0305d93f6d56f00c0cfda88983fd
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-08-06 16:25:26 +0300

    Fix typo

--- FormalTopology/generate_html.sh
+++ FormalTopology/generate_html.sh
@@ -7,7 +7,7 @@ for f in `ls *.lagda.md`; do
 done
 
 for f in `ls *.agda`; do
-    agda -css Agda.css --html --html-highlight=code $f
+    agda --css Agda.css --html --html-highlight=code $f
 done
 
 cp -f ../resources/Agda.css html/Agda.css
